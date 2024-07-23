426e2bb13613456cd2556d58d93a1be9d58e3fda
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-23 12:44:08 -0300

    Initial commit

---
+++ src/test/java/com/buongermino/parking_API/ParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buongermino.parking_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
