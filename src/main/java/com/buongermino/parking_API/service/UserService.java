554bb7bcc1ece001fa5fbbce08fb18e663cbb83a
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-24 09:17:03 -0300

    Added get by id and update password to service

--- src/main/java/com/buongermino/parking_API/service/UserService.java
+++ src/main/java/com/buongermino/parking_API/service/UserService.java
@@ -2,9 +2,11 @@ package com.buongermino.parking_API.service;
 
 import com.buongermino.parking_API.entities.User;
 import com.buongermino.parking_API.repositories.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,19 @@ public class UserService {
     @Transactional
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User getById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+            () -> new RuntimeException("User not found")
+        );
+    }
+
+    @Transactional
+    public User updatePassword(Long id, String password) {
+        User user = getById(id);
+        user.setPassword(password);
+        return user;
     }
 }
