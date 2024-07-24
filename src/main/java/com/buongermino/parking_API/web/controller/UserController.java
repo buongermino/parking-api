fe72f6a4da6c45b309614d05c72e80447f1c72ae
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-24 09:16:09 -0300

    Added Get and Patch methods to Controller

--- src/main/java/com/buongermino/parking_API/web/controller/UserController.java
+++ src/main/java/com/buongermino/parking_API/web/controller/UserController.java
@@ -5,10 +5,7 @@ import com.buongermino.parking_API.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -21,5 +18,17 @@ public class UserController {
     public ResponseEntity<User> create(@RequestBody User user) {
         User createdUser = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping ("/{id}")
+    public ResponseEntity<User> get(@PathVariable Long id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping ("/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User userRequest) {
+        User user = userService.updatePassword(id, userRequest.getPassword());
+        return ResponseEntity.ok(user);
     }
 }
